896e051e8a131bf79d63141fc53dfed3553c2356
Nicky van Urk <nickyvanurk@gmx.com>
2020-10-19 19:09:38 +0200

    Add monster chase player behaviour

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,21 +1,40 @@
-use super::{Monster, Name, Viewshed};
-use rltk::{console, Point};
+use super::{Map, Monster, Name, Position, Viewshed};
+use rltk::{a_star_search, console, Point};
 use specs::prelude::*;
 
 pub struct MonsterAiSystem {}
 
 impl<'a> System<'a> for MonsterAiSystem {
     type SystemData = (
+        WriteExpect<'a, Map>,
         ReadExpect<'a, Point>,
         WriteStorage<'a, Viewshed>,
         ReadStorage<'a, Monster>,
         ReadStorage<'a, Name>,
+        WriteStorage<'a, Position>,
     );
 
-    fn run(&mut self, (player_pos, viewshed, monster, name): Self::SystemData) {
-        for (viewshed, _, name) in (&viewshed, &monster, &name).join() {
+    fn run(
+        &mut self,
+        (mut map, player_pos, mut viewshed, monster, name, mut position): Self::SystemData,
+    ) {
+        for (mut viewshed, _, name, mut pos) in
+            (&mut viewshed, &monster, &name, &mut position).join()
+        {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(&format!("{} shouts insults", name.name));
+
+                let path = a_star_search(
+                    map.xy_idx(pos.x, pos.y) as i32,
+                    map.xy_idx(player_pos.x, player_pos.y) as i32,
+                    &mut *map,
+                );
+
+                if path.success && path.steps.len() > 1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
