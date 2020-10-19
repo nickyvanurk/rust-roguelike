25f7225fbfa1b6825ca14040c5771051646a1453
Nicky van Urk <nickyvanurk@gmx.com>
2020-10-20 00:58:50 +0200

    Add combat stats

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Monster>();
     gs.ecs.register::<Name>();
     gs.ecs.register::<BlocksTile>();
+    gs.ecs.register::<CombatStats>();
 
     let map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
@@ -112,6 +113,12 @@ fn main() -> rltk::BError {
         })
         .with(Name {
             name: "Player".to_string(),
+        })
+        .with(CombatStats {
+            max_hp: 30,
+            hp: 30,
+            defense: 2,
+            power: 5,
         })
         .build();
 
@@ -143,6 +150,12 @@ fn main() -> rltk::BError {
                 name: format!("{} #{}", &name, idx),
             })
             .with(BlocksTile {})
+            .with(CombatStats {
+                max_hp: 16,
+                hp: 16,
+                defense: 1,
+                power: 4,
+            })
             .build();
     }
 
