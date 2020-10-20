b3b23f0159a3297bc96a44f178a71d00fd43909d
Nicky van Urk <nickyvanurk@gmx.com>
2020-10-20 22:34:07 +0200

    Add melee attack logging

--- src/melee_combat_system.rs
+++ src/melee_combat_system.rs
@@ -1,4 +1,4 @@
-use super::{CombatStats, Name, SufferDamage, WantsToMelee};
+use super::{game_log::GameLog, CombatStats, Name, SufferDamage, WantsToMelee};
 use rltk::console;
 use specs::prelude::*;
 
@@ -7,6 +7,7 @@ pub struct MeleeCombatSystem {}
 impl<'a> System<'a> for MeleeCombatSystem {
     type SystemData = (
         Entities<'a>,
+        WriteExpect<'a, GameLog>,
         WriteStorage<'a, WantsToMelee>,
         ReadStorage<'a, Name>,
         ReadStorage<'a, CombatStats>,
@@ -15,7 +16,7 @@ impl<'a> System<'a> for MeleeCombatSystem {
 
     fn run(
         &mut self,
-        (entities, mut wants_melee, names, combat_stats, mut inflict_damage): Self::SystemData,
+        (entities, mut log, mut wants_melee, names, combat_stats, mut inflict_damage): Self::SystemData,
     ) {
         for (_, wants_melee, name, stats) in (&entities, &wants_melee, &names, &combat_stats).join()
         {
@@ -27,12 +28,12 @@ impl<'a> System<'a> for MeleeCombatSystem {
                     let damage = i32::max(0, stats.power - target_stats.defense);
 
                     if damage == 0 {
-                        console::log(&format!(
+                        log.entries.push(format!(
                             "{} is unable to hurt {}",
                             &name.name, &target_name.name
                         ));
                     } else {
-                        console::log(&format!(
+                        log.entries.push(format!(
                             "{} hits {} for {} hp.",
                             &name.name, &target_name.name, damage
                         ));
